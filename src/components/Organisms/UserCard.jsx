6a3ca63d7946c9f2157a66acc2a6fa90da97a1d7
yasuyuki sato <godasper@gmail.com>
2021-12-07 15:51:31 +0900

    UserCardコンポーネントを作成し、UserMangementコンポーネントをupdate

---
+++ src/components/Organisms/UserCard.jsx
@@ -0,0 +1,33 @@
+import { Image } from "@chakra-ui/image";
+import { Box, Stack, Text } from "@chakra-ui/layout";
+
+export const UserCard = (props) => {
+  const { imageUrl, userName, fullName } = props;
+  return (
+    <Box
+      w="260px"
+      h="260px"
+      bg="gray"
+      shadow="md"
+      borderRadius="10px"
+      p={4}
+      _hover={{ cursor: "pointer", opacity: 0.8 }}
+    >
+      <Stack textAlign="center">
+        <Image
+          borderRadius="full"
+          boxSize="160px"
+          src={imageUrl}
+          alt={fullName}
+          m="auto"
+        />
+      </Stack>
+      <Text fontSize="lg" fontWeight="bold">
+        {userName}
+      </Text>
+      <Text fontSize="sm" color="white">
+        {fullName}
+      </Text>
+    </Box>
+  );
+};
--- src/components/Pages/UserManagement.jsx
+++ src/components/Pages/UserManagement.jsx
@@ -1,3 +1,18 @@
+import { Wrap, WrapItem } from "@chakra-ui/layout";
+import { UserCard } from "../Organisms/UserCard";
+
 export const UserManagement = () => {
-  return <p>スタッフ管理ページ</p>;
+  return (
+    <>
+      <Wrap P={{ base: 4, md: 10 }}>
+        <WrapItem>
+          <UserCard
+            imageUrl="https://source.unsplash.com/random"
+            userName="ニックネーム"
+            fullName="名前"
+          />
+        </WrapItem>
+      </Wrap>
+    </>
+  );
 };
