f1dac87f7b5332fb1bcd1c65378580c4e00e0387
yasuyuki sato <godasper@gmail.com>
2021-12-08 01:11:45 +0900

    ユーザー一覧取得の機能を実装

--- src/components/Pages/UserManagement.jsx
+++ src/components/Pages/UserManagement.jsx
@@ -1,18 +1,35 @@
 import { Wrap, WrapItem } from "@chakra-ui/layout";
+import { Spinner } from "@chakra-ui/spinner";
+import { useEffect } from "react";
+import { useFetchUseAllUsers } from "../../hooks/useFetchUseAllUsers";
 import { UserCard } from "../Organisms/UserCard";
 
 export const UserManagement = () => {
+  const { getAllUsers, loading, users } = useFetchUseAllUsers();
+
+  // 全ユーザー取得の関数を実行
+  // 初回時のみレンダリングされるようにuseEffectを使用
+  useEffect(() => {
+    getAllUsers();
+  }, []);
+
   return (
     <>
-      <Wrap P={{ base: 4, md: 10 }}>
-        <WrapItem>
-          <UserCard
-            imageUrl="https://source.unsplash.com/random"
-            userName="ニックネーム"
-            fullName="名前"
-          />
-        </WrapItem>
-      </Wrap>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <Wrap P={{ base: 4, md: 10 }}>
+          {users.map((user) => (
+            <WrapItem key={user.id}>
+              <UserCard
+                imageUrl="https://source.unsplash.com/random"
+                userName={user.username}
+                fullName={user.name}
+              />
+            </WrapItem>
+          ))}
+        </Wrap>
+      )}
     </>
   );
 };
