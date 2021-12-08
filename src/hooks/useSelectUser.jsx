7d55e8eda593b94de7ba6ef729acec1e7068ad08
yasuyuki sato <godasper@gmail.com>
2021-12-08 17:57:44 +0900

    選択したユーザーを特定し、モーダルを表示するカスタムフックを作成

---
+++ src/hooks/useSelectUser.jsx
@@ -0,0 +1,16 @@
+//　選択したユーザーを特定し、モーダルを表示するカスタムフック
+import { useCallback, useState } from "react";
+
+export const useSelectUser = () => {
+  // 選択されたユーザーの情報を保持するステート
+  const [selectedUser, setSelectedUser] = useState();
+
+  // ユーザーがクリックされたときにユーザーを返す関数
+  const onSelectUser = useCallback((props) => {
+    const { users, id, onOpen } = props;
+    const targetUser = users.find((user) => user.id === id);
+    setSelectedUser(targetUser);
+    onOpen();
+  }, []);
+  return { onSelectUser, selectedUser };
+};
